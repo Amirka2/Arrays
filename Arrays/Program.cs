cbee7e513ec5b2ef680df04fa903f156906956d1
Амир <far20022002213@gmail.com>
2020-10-23 14:27:25 +0500

    1

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -5,29 +5,74 @@ namespace Arrays
 {
     internal class Program
     {
-        public static string ReadArrays(string number)
+        public static int Check(string text)
         {
-            Console.WriteLine($"Введите {number} массив");
-            string arr = Console.ReadLine();
-            return arr;
+            int num;
+            Console.WriteLine($"{text}");
+            string el = Console.ReadLine();
+            bool gotcha = int.TryParse(el, out num);
+            
+            while (gotcha == false)
+            {
+                Console.WriteLine("Нужно ввести число!");
+                el = Console.ReadLine();
+                gotcha = int.TryParse(el, out num);
+            } 
+
+            return num;
+        }
+        public static int[] ReadArray(string number)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            var length = Check($"Введите количество элементов {number} массива");
+            Console.ResetColor();
+            
+            int[] array = new int[length];
+            
+            for (int i = 0; i < length; ++i)
+            {
+                array[i] = Check($"Введите {i+1} элемент {number} массива");
+            }
+
+            return array;
         }
 
-        public static string ArrayConverter(string arr)
+        public static void ArrayComparsion(int[] arr1, int[] arr2)
         {
-            var strArr = arr.Split('[', ']', ' ');
-            arr = String.Concat(strArr);
-            return arr;
+            if (arr1.Length < arr2.Length)
+            {
+                Console.WriteLine("Первый массив меньше второго!");
+            }
+            else
+            {
+                int index2 = 0;
+                for (int index1 = 0; index1 < arr1.Length; index1++)
+                {
+                    if ((arr2[index2] == arr1[index1]) 
+                        && ((arr1.Length - index1) >= (arr2.Length - index2))
+                        && ((arr2.Length - index2) > 0))
+                    {
+                        if (arr2.Length - index2 - 1 == 0)
+                        {
+                            Console.WriteLine("Первый массив содержит в себе второй массив");
+                        }
+                        index2++;
+                        break;
+                    }
+                    else if (arr2[index2] != arr1[index1])
+                    {
+                        Console.WriteLine("Первый массив не содержит в себе второй массив");
+                        break;
+                    }
+                }
+            }
         }
         
         public static void Main(string[] args)
         {
-            var arr1 = ReadArrays("1");
-            var arr2 = ReadArrays("2");
-
-            arr1 = ArrayConverter(arr1);
-            arr2 = ArrayConverter(arr2);
-
-            Console.WriteLine(arr1.Contains(arr2));
+            var arr1 = ReadArray("1");
+            var arr2 = ReadArray("2");
+            ArrayComparsion(arr1, arr2);
         }
     }
 }
